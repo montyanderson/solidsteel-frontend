4451661910430ffab5ea8107ce236b3aa19cbe25
Paul Carvill <paul@paulcarvill.com>
2015-02-24 01:08:25 +0000

    initial commit

---
+++ app/models/broadcast.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  session_name: DS.attr('string'),
+  broadcast_date: DS.attr('date'),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date')
+});
