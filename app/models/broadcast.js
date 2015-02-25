141cbdad20be7119cde3b2e040062e9ca77cb1d5
Paul Carvill <paul@paulcarvill.com>
2015-02-25 00:43:48 +0000

    adding broadcast/mix relations in front-end

--- app/models/broadcast.js
+++ app/models/broadcast.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   session_name: DS.attr('string'),
   broadcast_date: DS.attr('date'),
   createdAt: DS.attr('date'),
-  updatedAt: DS.attr('date')
+  updatedAt: DS.attr('date'),
+  mixes: DS.hasMany('mix', {embedded: 'always'})
 });
--- app/models/mix.js
+++ app/models/mix.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  part: DS.attr()
+  part: DS.attr(),
+  broadcast: DS.belongsTo('broadcast')
 });
