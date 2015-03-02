04eaacad3e16e15a9981de03956256ef06386ab2
Paul Carvill <paul@paulcarvill.com>
2015-03-02 18:46:32 +0000

    setting up broadcats year filtering

--- app/routes/broadcasts/index.js
+++ app/routes/broadcasts/index.js
@@ -1,7 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  controllerName: "broadcasts",
+
+  setupController: function(controller, model) {
+  	console.log(controller);
+    controller.set('model', model);
+  },
+
   model: function(params) {
-    return this.store.find('broadcast', {year: params.year});
+  	console.log('in model: ' + this.get("controller.year"));
+    return this.store.find('broadcast', {year: this.get('controller.year')});
+  },
+
+  actions: {
+    queryParamsDidChange: function(params) {
+      console.log("queryParamsDidChange : was " + this.get('controller.year')); 
+      console.log("queryParamsDidChange : will be " + (params.year || 'default value'));
+
+      //this.get('controller').set('year', params.year)
+      this.refresh();
+    }
   }
+
 });
