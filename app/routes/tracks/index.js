c9766e3552dab97ae9fc689ee860107c2ea1b7cf
Paul Carvill <paul@paulcarvill.com>
2015-03-07 13:43:57 +0000

    setting broadcast css when tracks route is directly loaded

--- app/routes/tracks/index.js
+++ app/routes/tracks/index.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	needs: ['broadcast'],
 
+	beforeModel : function(){
+		this.controllerFor('broadcast').set('showingTracks', true).set('plusMinus', '–');
+	}
 });
