8e404635a089f4275b6aa98fdf3b80e4d5327d26
Paul Carvill <paul@paulcarvill.com>
2015-03-21 15:39:49 +0000

    adjusted play audiowave on mobile

--- app/models/mix.js
+++ app/models/mix.js
@@ -17,7 +17,8 @@ Mix.reopenClass({FIXTURES : [
 		name: "mix a",
 		tracks: [1, 2, 3],
 		url: "293",
-		img: "bonobo.jpg"
+		img: "bonobo.jpg",
+		synopsis: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	},
 	{
 		id: 2,
