6176e7f77c3cf336bbfcbd63a61c8f468d90d15d
Paul Carvill <paul@paulcarvill.com>
2015-03-21 12:49:01 +0000

    syling for landing page

--- app/routes/broadcast.js
+++ app/routes/broadcast.js
@@ -15,23 +15,18 @@ export default Ember.Route.extend({
 
   setupController: function(controller, model){
     if(controller.get('currentlyPlaying')){
-      console.log('yes there is a soudn playing');
       controller.stopCurrentMix();
     } else {
-      console.log('nothing playgin eigt now');
     }
 
     if(!model.get('mixes').get('length')) {
-      console.log('gonna load mix models...');
       model.reload().then(function(){
-        
         controller.set('model', model);
         controller.setCurrentMix();
       });
     }
     
     else {
-      console.log('standard');
       controller.set('model', model);
       controller.setCurrentMix();
     }
