1813c614ac2d46ca54e15aa44cef734b22789bd4
Paul Carvill <paul@paulcarvill.com>
2015-03-22 21:27:21 +0000

    closing sidepanel when navigating directly away from tracklisting

--- app/routes/tracks/index.js
+++ app/routes/tracks/index.js
@@ -5,5 +5,13 @@ export default Ember.Route.extend({
 
 	beforeModel : function(){
 		this.controllerFor('broadcast').set('showingTracks', true).set('plusMinus', '–');
-	}
+	},
+
+	 actions: {
+	 	willTransition: function(transition) {
+	      this.controllerFor('application').set('showingTracks', 'false');
+	      this.controllerFor('broadcast').set('showingTracks', 'false');
+	      this.controllerFor('broadcast').set('plusMinus', '+');
+	    }
+	 }
 });
