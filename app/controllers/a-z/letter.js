8d0efc70dfafc5cb92f467bcbbabad5c492c8442
Paul Carvill <paul@paulcarvill.com>
2015-03-25 01:04:00 +0000

    styling for a-z pages and navigation

--- app/controllers/a-z/letter.js
+++ app/controllers/a-z/letter.js
@@ -2,5 +2,52 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  letter: '',
+
+  dist: null,
+
+  wasSwiped: false,
+
+  actions: {
+
+    swipe: function(){
+
+        if(this.dist == undefined) {
+            this.dist = Ember.$('.alpha-index').width();
+        }
+
+        var currentOffset = parseInt(Ember.$('.alpha-index').css('left'));
+
+        Ember.$('.alpha-index').animate({
+            'left': (currentOffset + -this.dist)
+        }, 1000);
+
+        this.set('wasSwiped', true);
+
+    },
+
+    backswipe: function(){
+        if(this.dist == undefined) {
+            this.dist = Ember.$('.alpha-index').width();
+        }
+
+        var currentOffset = parseInt(Ember.$('.alpha-index').css('left'));
+
+        Ember.$('.alpha-index').animate({
+            'left': (currentOffset + 260)
+        }, 1000);
+
+        this.set('wasSwiped', true);
+    },
+
+    resetswipe: function(){
+        if (this.get('wasSwiped')) {
+            Ember.$('.alpha-index').offset({
+                'left': '176'
+            });
+        }
+    }
+
+  }
   
 });
--- app/routes/a-z/letter.js
+++ app/routes/a-z/letter.js
@@ -3,6 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   	model: function(params) {
+  	  this.set('letter', params.letter);
 	  return this.store.find('dj', {alpha: params.letter});
-	}
+	},
+
+	setupController: function(controller, model) {
+  	  controller.set('model', model);
+      controller.set('letter', this.letter);
+    },
+
+    actions: {
+        willTransition: function(transition) {
+        this.get('controller').send('resetswipe');
+        }
+     }
 });
