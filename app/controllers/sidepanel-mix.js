751c9016e31be8f644d970ac63bc92f6fd701d99
Paul Carvill <paul@paulcarvill.com>
2015-03-29 15:01:01 +0100

    work on showing tracks in timeline view

--- app/controllers/sidepanel-mix.js
+++ app/controllers/sidepanel-mix.js
@@ -3,8 +3,16 @@ import ENV from '../config/environment';
 
 export default Ember.Controller.extend({
   
+  needs: ['broadcasts'],
+
   rootpath: function(){
     return ENV.APP.API_HOST;
-  }.property('rootpath')
+  }.property('rootpath'),
 
+  actions: {
+    sneakTracks: function(){
+        this.model.reload();
+    }
+
+  }
 });
--- app/views/sidepanel-mix.js
+++ app/views/sidepanel-mix.js
@@ -10,20 +10,35 @@ export default Ember.View.extend({
 
   hover: false,
 
+  hasTracks: false,
+
+  top: false,
+
   mouseEnter: function() { this.set('hover', true); },
   mouseLeave: function() { this.set('hover', false); },
 
   click: function(e){
-    // scroll element to top of sidepanel
-    var nearestMix = Ember.$(e.target).closest('.mix-in-sidepanel').offset().top;
-    var container = Ember.$(".sidepanel").scrollTop();
-    Ember.$(".sidepanel").animate({scrollTop: nearestMix+container}, 500);
 
-    // show tracklist below it
+    if(!this.get('top')){
+      // scroll element to top of sidepanel
+      var nearestMix = Ember.$(e.target).closest('.mix-in-sidepanel').offset().top;
+      var container = Ember.$(".sidepanel").scrollTop();
+      Ember.$(".sidepanel").animate({scrollTop: nearestMix+container}, 500);
+    }
 
-    // hide plus, show minus
-
-    
+    if(!this.get('hasTracks')) {
+      this.set('top', true);
+      // show tracklist below it
+      this.get('controller').send('sneakTracks', this.get('controller.model'));
+      // hide plus, show minus
+      this.set('hasTracks', true);
+      // extend div to show tracks
+      //Ember.$(e.target).closest('.mix-in-sidepanel').css('height', '100%');
+    } else {
+      this.set('top', false);
+      this.set('hasTracks', false);
+      //Ember.$(e.target).closest('.mix-in-sidepanel').css('height', '94px');
+    }
   }
 
 });
