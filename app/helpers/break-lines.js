9d1dd58e68eac0041d29b9a909f1116df661544a
Paul Carvill <paul@paulcarvill.com>
2015-05-01 13:13:01 +0100

    enabling html in synopsis

--- app/helpers/break-lines.js
+++ app/helpers/break-lines.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function breakLines(text){
-    text = Ember.Handlebars.Utils.escapeExpression(text);
+    //text = Ember.Handlebars.Utils.escapeExpression(text);
     text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
     return new Ember.Handlebars.SafeString(text);
 }
